fa952c82246d6c848701be4b56e6d77958b6dad8
leo thllz <96989505+Leothllz@users.noreply.github.com>
2023-11-08 12:31:46 +0100

    fonction de recherche

--- rechercheAvancees.py
+++ rechercheAvancees.py
@@ -7,7 +7,12 @@ def rechercher():
         "genre": genre_var.get(),
         "race": race_var.get(),
         "puissance_min": entry_puissance_min.get(),
-        "puissance_max": entry_puissance_max.get()
+        "puissance_max": entry_puissance_max.get(),
+        "intelligent": intelligent_var.get(),
+        "rapide": rapide_var.get(),
+        "durabilite": durabilite_var.get(),
+        "pouvoir": pouvoir_var.get(),
+        "combat": combat_var.get()
     }
 
     with open('all.json', 'r') as json_file:
@@ -27,6 +32,18 @@ def rechercher():
         if critere["puissance_max"] and critere["puissance_max"] < superheros["powerstats"]["strength"]:
             continue
 
+        # Vérifier les critères supplémentaires
+        if critere["intelligent"] and superheros["powerstats"]["intelligence"] < 50:
+            continue
+        if critere["rapide"] and superheros["powerstats"]["speed"] < 50:
+            continue
+        if critere["durabilite"] and superheros["powerstats"]["durability"] < 50:
+            continue
+        if critere["pouvoir"] and superheros["powerstats"]["power"] < 50:
+            continue
+        if critere["combat"] and superheros["powerstats"]["combat"] < 50:
+            continue
+
         resultats.append(superheros)
 
     # Afficher les résultats dans une zone de texte
@@ -38,6 +55,11 @@ def rechercher():
 
 def afficher_filtres_supplementaires():
     frame_filtres_supplementaires.pack()
+    button_filtres.config(text="Masquer les filtres")
+
+def cacher_filtres_supplementaires():
+    frame_filtres_supplementaires.pack_forget()
+    button_filtres.config(text="Plus de filtres")
 
 # Créer la fenêtre principale
 window = tk.Tk()
@@ -58,17 +80,16 @@ label_puissance_min = tk.Label(window, text="Puissance minimale:")
 entry_puissance_min = tk.Entry(window)
 label_puissance_max = tk.Label(window, text="Puissance maximale:")
 entry_puissance_max = tk.Entry(window)
+button_filtres = tk.Button(window, text="Plus de filtres", command=afficher_filtres_supplementaires)
 
 # Créer un bouton pour la recherche
 button_rechercher = tk.Button(window, text="Rechercher", command=rechercher)
-button_filtres = tk.Button(window, text="Plus de filtres", command=afficher_filtres_supplementaires)
 
 # Créer une zone de texte pour afficher les résultats
 resultat_text = tk.Text(window, state=tk.DISABLED)
 
 # Créer un cadre pour les filtres supplémentaires
 frame_filtres_supplementaires = tk.Frame(window)
-frame_filtres_supplementaires.pack_forget()
 
 # Ajouter des critères supplémentaires au cadre
 label_critere_supplementaire = tk.Label(frame_filtres_supplementaires, text="Critères supplémentaires:")
@@ -83,6 +104,21 @@ checkbox_intelligent.pack()
 rapide_var = tk.IntVar()
 checkbox_rapide = tk.Checkbutton(frame_filtres_supplementaires, text="Rapide", variable=rapide_var)
 checkbox_rapide.pack()
+
+# Exemple de critère supplémentaire : Une case à cocher pour les superhéros avec une durabilité élevée
+durabilite_var = tk.IntVar()
+checkbox_durabilite = tk.Checkbutton(frame_filtres_supplementaires, text="Durabilité élevée", variable=durabilite_var)
+checkbox_durabilite.pack()
+
+# Exemple de critère supplémentaire : Une case à cocher pour les superhéros avec un pouvoir élevé
+pouvoir_var = tk.IntVar()
+checkbox_pouvoir = tk.Checkbutton(frame_filtres_supplementaires, text="Pouvoir élevé", variable=pouvoir_var)
+checkbox_pouvoir.pack()
+
+# Exemple de critère supplémentaire : Une case à cocher pour les superhéros avec un combat élevé
+combat_var = tk.IntVar()
+checkbox_combat = tk.Checkbutton(frame_filtres_supplementaires, text="Combat élevé", variable=combat_var)
+checkbox_combat.pack()
 
 # Placer les éléments dans la fenêtre
 label_nom.pack()
