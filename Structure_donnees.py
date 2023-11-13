66455ee5cb113ed301f150842b9bd6b2d3f16055
Cyrius <debuche.cyril@outlook.com>
2023-11-14 00:35:22 +0100

    Ajout de tous les commentaires nécessaires dans le code pour la bonne compréhension. Etape n°6 terminée

--- Structure_donnees.py
+++ Structure_donnees.py
@@ -1,7 +1,7 @@
 import json
 import sys
 
-
+# Definition de la structure de données sous forme de dictionnaire
 class SuperHero:
     def __init__(self, data):
         self.id = data["id"]
@@ -59,6 +59,9 @@ class Images:
         self.md = data["md"]
         self.lg = data["lg"]
 
+
+# Définition de l'objet liste de super héros, permettant d'y inclure une fonction pour ajouter
+# un super Héro dans une liste
 class SuperHeroList:
     def __init__(self):
         self.superheroes = []
@@ -66,60 +69,76 @@ class SuperHeroList:
     def add_superhero(self, superhero):
         self.superheroes.append(superhero)
 
+
+# Définition du sous programme pour lire les données du fichier JSON avec plusieurs vérifications pour
+# éviter les erreurs de chargement du fichier ainsi que des vérifications sur la structure des données
+# dans le fichier JSON
+
 def load_superheroes_from_json(file_path):
     try:
+        # Lecture du fichier JSON
         with open(file_path, "r") as file:
             data = json.load(file)
+
     except FileNotFoundError:
-        print(f"Error: File '{file_path}' not found.")
+        # Si le prgramme ne trouve pas de fichier dans le chemin indiqué
+        print(f"Erreur : Fichier '{file_path}' introuvable.")
         return None
+
     except json.JSONDecodeError:
-        print(f"Error: Invalid JSON format in file '{file_path}'.")
+        #Format incorrect
+        print(f"Erreur : Format JSON invalide dans le fichier '{file_path}'.")
         return None
+
     except Exception as e:
-        print(f"Error: An unexpected error occurred - {e}.")
+        # Erreur inconnue
+        print(f"Erreur : Une erreur inattendu s'est produite - {e}.")
         return None
 
     if not isinstance(data, list):
-        print("Error: JSON data should be a list of superheroes.")
+        # Les données ne sont pas sous la forme d'un dictionnaire
+        print("Erreur : Les données JSON devraient être une liste de super héros.")
         return None
 
+    # Si toutes les vérifications sont passées, création de la liste de super héros
     superhero_list = SuperHeroList()
 
     for superhero_data in data:
         if not isinstance(superhero_data, dict):
-            print("Error: Each superhero data should be a dictionary.")
+            print("Erreur : Chaque données d'un super héro devraient être un dictionnaire.")
             return None
 
         try:
             superhero = SuperHero(superhero_data)
             superhero_list.add_superhero(superhero)
+
         except KeyError as ke:
-            print(f"Error: Missing key {ke} in superhero data.")
+            print(f"Erreur: Clé manquante {ke} dans les données du super héro.")
             return None
+
         except Exception as e:
-            print(f"Error: An unexpected error occurred - {e}.")
+            print(f"Erreur : Une erreur inattendu s'est produite - {e}.")
             return None
 
         # Valider la structure des données pour chaque super-héros
         if not all(key in superhero_data for key in ["id", "name", "slug", "powerstats", "appearance", "biography", "work", "connections", "images"]):
-            print("Error: Incomplete superhero data structure.")
+            print("Erreur: La structure de données du super héro est incomplète.")
             return None
-
-        # Ajoutez des validations supplémentaires pour chaque section (powerstats, appearance, etc.) si nécessaire.
 
     return superhero_list
 
 
 # Exemple d'utilisation :
-json_file_path = "api/all.json"
+json_file_path = "api/all.json"  # Mettre le chemin source du fichier JSON
 superhero_list = load_superheroes_from_json(json_file_path)
 
 if superhero_list is not None:
     # Les données ont été chargées avec succès
     # Accéder à la liste de super-héros
     print("Les données ont été chargées avec succès, voici le nom de tous les super héros :")
+
     for superhero in superhero_list.superheroes:
+        # Affiche le nom de tous les super héros
         print(superhero.name)
     pass
 else:
@@ -128,8 +147,10 @@ else:
     pass
 
 def modify_superhero(json_file_path):
+
     superhero_name = input("Entrez le nom du super héros à modifier : ")
 
+    # Lecture du fichier JSON
     with open(json_file_path, "r") as file:
         data = json.load(file)
 
@@ -141,11 +162,12 @@ def modify_superhero(json_file_path):
             print(f"Modification des données pour {superhero_name}")
             field_path = input("Entrez le chemin du champ (exemple, 'name' or 'powerstats.speed') : ")
 
-            # Split the field path into individual fields
+            # Divise les champs imbriqués en champs individuels
             field_path_list = field_path.split('.')
             current_data = superhero_data
 
-            # Traverse the nested structure to find the target field
+            # Parcoure les structures imbriqués pour savoir si le champ souhaité existe, exemple si on veut modifier
+            # powerstats.speed
             for field in field_path_list[:-1]:
                 if field in current_data:
                     current_data = current_data[field]
@@ -153,16 +175,16 @@ def modify_superhero(json_file_path):
                     print(f"{field} n'est pas un champ valide pour {superhero_name}")
                     return
 
-            # Check if the last field in the path exists in the current_data
+            # Vérifies si le champ souhaité à modifier existe bien dans la structure de données d'un super héro
             last_field = field_path_list[-1]
             if last_field not in current_data:
                 print(f"Erreur: {last_field} n'est pas un champ valide pour {superhero_name}.")
                 return
 
-            # Modify the final field (except for the last field in the path)
+            # Modifies l'information souhaitée dans les données du super héro
             new_value = input(f"Entrez la nouvelle valeur pour {last_field} : ")
 
-            # Convert new value to integer if the original field is an integer
+            # Convertie la nouvelle en int si la valeur originale était aussi un int
             if isinstance(current_data[last_field], int):
                 new_value = int(new_value)
 
@@ -170,7 +192,7 @@ def modify_superhero(json_file_path):
             print(f"{last_field} a été modifié par {new_value}")
 
     if not superhero_found:
-        print(f"Erreur: Aucun super héros trouvé avec le nom {superhero_name}.")
+        print(f"Erreur: Aucun super héro trouvé avec le nom {superhero_name}.")
 
     with open(json_file_path, "w") as file:
         json.dump(data, file, indent=2)
@@ -178,4 +200,3 @@ def modify_superhero(json_file_path):
 
 # Exemple d'utilisation :
 modify_superhero(json_file_path)
-
